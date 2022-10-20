d06dd154f4f28e99e3c9db2766067fe7dfe8bf40
RHRiday <riffatriday@gmail.com>
2022-10-20 21:40:30 +0600

    v1.2: mobile view

--- src/components/TheWelcome.vue
+++ src/components/TheWelcome.vue
@@ -1,9 +1,9 @@
 <template>
   <div class="row justify-content-between">
-    <div class="col-4 resume-wrapper">
+    <div class="col-xl-4 resume-wrapper">
       <ResumeForm @data="getFormData" />
     </div>
-    <div class="col-8">
+    <div class="col-xl-8">
       <article
         class="resume-wrapper text-right position-relative"
         id="convertToPdf"
@@ -12,7 +12,7 @@
           <header class="resume-header pt-4 pt-md-0">
             <div class="row">
               <div
-                class="col-block col-md-auto resume-picture-holder text-center text-md-start"
+                class="col-4 col-xl-auto resume-picture-holder text-center text-xl-start"
               >
                 <img
                   class="picture"
@@ -22,10 +22,8 @@
               </div>
               <!--//col-->
               <div class="col">
-                <div
-                  class="row p-4 justify-content-center justify-content-md-between"
-                >
-                  <div class="primary-info col-auto">
+                <div class="row p-xl-4 justify-content-between">
+                  <div class="primary-info col-7 col-xl-auto">
                     <h1
                       class="name mt-0 mb-1 text-white text-uppercase text-uppercase"
                     >
@@ -80,11 +78,12 @@
                     </ul>
                   </div>
                   <!--//primary-info-->
-                  <div class="secondary-info col-auto mt-2">
+                  <div class="secondary-info col-5 col-xl-auto mt-2">
                     <ul class="resume-social list-unstyled">
                       <li class="mb-3" v-for="site in resumeData.websites">
                         <a class="text-link" :href="site" target="_blank"
-                          ><span class="fa-container text-center me-2"
+                          ><span
+                            class="fa-container text-center me-2 d-none d-xl-inline-block"
                             ><i
                               :class="getIcon(site) + ' text-muted fa-fw'"
                             ></i></span
@@ -101,10 +100,10 @@
             </div>
             <!--//row-->
           </header>
-          <div class="resume-body p-5">
-            <section class="resume-section summary-section mb-5">
+          <div class="resume-body p-3 p-lg-5">
+            <section class="resume-section summary-section mb-3 mb-xl-5">
               <h2
-                class="resume-section-title text-uppercase font-weight-bold pb-3 mb-3"
+                class="resume-section-title text-uppercase font-weight-bold pb-lg-3 mb-lg-3"
               >
                 Career Summary
               </h2>
@@ -117,9 +116,9 @@
             <!--//summary-section-->
             <div class="row">
               <div class="col-lg-9">
-                <section class="resume-section experience-section mb-5">
+                <section class="resume-section experience-section mb-3 mb-xl-5">
                   <h2
-                    class="resume-section-title text-uppercase font-weight-bold pb-3 mb-3"
+                    class="resume-section-title text-uppercase font-weight-bold pb-lg-3 mb-lg-3"
                   >
                     Work Experience
                   </h2>
@@ -130,7 +129,7 @@
                         class="resume-timeline-item position-relative pb-2"
                       >
                         <div class="resume-timeline-item-header mb-2">
-                          <div class="d-flex flex-column flex-md-row">
+                          <div class="d-flex">
                             <h3
                               class="resume-position-title font-weight-bold mb-1"
                             >
@@ -226,9 +225,9 @@
                 <!--//experience-section-->
               </div>
               <div class="col-lg-3">
-                <section class="resume-section skills-section mb-4">
+                <section class="resume-section skills-section mb-3 mb-xl-4">
                   <h2
-                    class="resume-section-title text-uppercase font-weight-bold pb-3 mb-3"
+                    class="resume-section-title text-uppercase font-weight-bold pb-lg-3 mb-lg-3"
                   >
                     Skills &amp; Tools
                   </h2>
@@ -237,8 +236,13 @@
                       <!-- <h4 class="resume-skills-cat font-weight-bold">
                         Frontend
                       </h4> -->
-                      <ul class="list-unstyled mb-4">
-                        <li class="mb-2" v-for="skill in resumeData?.skills">
+                      <ul
+                        class="list-unstyled mb-4 row d-lg-block justify-content-between"
+                      >
+                        <li
+                          class="mb-2 col-6 col-lg-12"
+                          v-for="skill in resumeData?.skills"
+                        >
                           <div class="resume-skill-name">{{ skill.tool }}</div>
                           <div class="progress resume-progress">
                             <div
@@ -255,9 +259,9 @@
                   <!--resume-section-content-->
                 </section>
                 <!--//skills-section-->
-                <section class="resume-section education-section mb-5">
+                <section class="resume-section education-section mb-3 mb-xl-5">
                   <h2
-                    class="resume-section-title text-uppercase font-weight-bold pb-3 mb-3"
+                    class="resume-section-title text-uppercase font-weight-bold pb-lg-3 mb-lg-3"
                   >
                     Education
                   </h2>
@@ -278,9 +282,9 @@
                   </div>
                 </section>
                 <!--//education-section-->
-                <section class="resume-section reference-section mb-5">
+                <section class="resume-section reference-section mb-3 mb-xl-5">
                   <h2
-                    class="resume-section-title text-uppercase font-weight-bold pb-3 mb-3"
+                    class="resume-section-title text-uppercase font-weight-bold pb-lg-3 mb-lg-3"
                   >
                     Awards & Certifications
                   </h2>
@@ -341,132 +345,132 @@ export default {
     };
   },
   mounted() {
-    //     this.resumeData = {
-    //       fullName: "Rifat Hossen Riday",
-    //       title: "Software Engineer",
-    //       email: "riffatriday@gmail.com",
-    //       phone: "1521401417",
-    //       img: "",
-    //       location: "Halishahar, Khan Bari, Chattogram 4216.",
-    //       summary: `A self-taught web developer with 3 years of personal experience and 1 year of
-    // professional experience working with web technologies. I have worked in 8+
-    // professional projects as well as 20+ personal projects with technologies such as
-    // php/laravel, node.js/angular/react.js using MVC architecture. I have also worked with
-    // APIs professionally to build customized ERP/CRM/LMS based applications.`,
-    //       websites: [
-    //         "https://rhriday.github.io/",
-    //         "https://github.com/RHRiday",
-    //         "https://linkedin.com/in/rhriday",
-    //       ],
-    //       experiences: [
-    //         {
-    //           title: "Software Engineer (Fullstack)",
-    //           company: "Panacea PLT",
-    //           desc: `• Designed front-end, developed APIs and hosted web applications in the
-    // required environment according to client needs.
-    // • Implemented special features to satisfy clients’ every day usage and easy to
-    // handle functionalities.
-    // • Had meetings with team and clients to ensure best project managing criteria
-    // are met while developing projects`,
-    //           startDate: "2021-09-12",
-    //           endDate: "Present",
-    //         },
-    //         {
-    //           title: "Jr. Web Developer",
-    //           company: "Zorg IT Group",
-    //           desc: `• Designed front-end, developed APIs and hosted web applications in the
-    // required environment according to client needs.
-    // • Implemented special features to satisfy clients’ every day usage and easy to
-    // handle functionalities.
-    // • Had meetings with team and clients to ensure best project managing criteria
-    // are met while developing projects.`,
-    //           startDate: "2021-08-02",
-    //           endDate: "2021-08-30",
-    //         },
-    //         {
-    //           title: "Laravel Developer (Intern)",
-    //           company: "PONDITs",
-    //           desc: `• Worked on projects using Laravel knowledge gained in classes to fix bugs
-    // and achieved development recognition by designing and implementing
-    // special project during internship.`,
-    //           startDate: "2021-08-02",
-    //           endDate: "2021-08-30",
-    //         },
-    //         {
-    //           title: "Tams Agency",
-    //           company: "Project",
-    //           desc: `• Integrated b2b portal among Airline companies with their APIs to cross-connect
-    // both businesses.
-    // • Worked on building REST APIs and took part as project manager. Also held meetings
-    // with client and front-end team on a regular basis.
-    // • Built special features according to clients’ needs such as admin, agent, sub-agent
-    // separate authorization; ticket booking, generating tickets, managing loans and payments,
-    // quick import/export in csv/pdf, admin control over all transactions etc.`,
-    //           startDate: "2022-02-05",
-    //           endDate: "2022-07-30",
-    //         },
-    //       ],
-    //       skills: [
-    //         {
-    //           tool: "Problem Solving/ Programming",
-    //           area: "83",
-    //         },
-    //         {
-    //           tool: "php/ Laravel",
-    //           area: "92",
-    //         },
-    //         {
-    //           tool: "Frontend development",
-    //           area: "87",
-    //         },
-    //         {
-    //           tool: "Backend development",
-    //           area: "95",
-    //         },
-    //         {
-    //           tool: "REST API",
-    //           area: "89",
-    //         },
-    //         {
-    //           tool: "Project management/ Team lead",
-    //           area: "71",
-    //         },
-    //         {
-    //           tool: "MySql, MongoDB",
-    //           area: "85",
-    //         },
-    //         {
-    //           tool: "Vue, React, Angular",
-    //           area: "80",
-    //         },
-    //         {
-    //           tool: "Adobe Photoshop",
-    //           area: "89",
-    //         },
-    //         {
-    //           tool: "Git/ Version control",
-    //           area: "83",
-    //         },
-    //         {
-    //           tool: "vsCode, Jira, Slack",
-    //           area: "83",
-    //         },
-    //       ],
-    //       education: [
-    //         {
-    //           degree: "Bsc in Computer Science and Engineering",
-    //           university: "International Islamic Univeristy, Chattogram",
-    //           grade: "3.5",
-    //           batch: "2021-10-05",
-    //         },
-    //       ],
-    //       awards: [
-    //         {
-    //           title: "Complete Web Development Bootcamp",
-    //           school: "Udemy",
-    //         },
-    //       ],
-    //     };
+    this.resumeData = {
+      fullName: "Rifat Hossen Riday",
+      title: "Software Engineer",
+      email: "riffatriday@gmail.com",
+      phone: "1521401417",
+      img: "",
+      location: "Halishahar, Khan Bari, Chattogram 4216.",
+      summary: `A self-taught web developer with 3 years of personal experience and 1 year of
+    professional experience working with web technologies. I have worked in 8+
+    professional projects as well as 20+ personal projects with technologies such as
+    php/laravel, node.js/angular/react.js using MVC architecture. I have also worked with
+    APIs professionally to build customized ERP/CRM/LMS based applications.`,
+      websites: [
+        "https://rhriday.github.io/",
+        "https://github.com/RHRiday",
+        "https://linkedin.com/in/rhriday",
+      ],
+      experiences: [
+        {
+          title: "Software Engineer (Fullstack)",
+          company: "Panacea PLT",
+          desc: `• Designed front-end, developed APIs and hosted web applications in the
+    required environment according to client needs.
+    • Implemented special features to satisfy clients’ every day usage and easy to
+    handle functionalities.
+    • Had meetings with team and clients to ensure best project managing criteria
+    are met while developing projects`,
+          startDate: "2021-09-12",
+          endDate: "Present",
+        },
+        {
+          title: "Jr. Web Developer",
+          company: "Zorg IT Group",
+          desc: `• Designed front-end, developed APIs and hosted web applications in the
+    required environment according to client needs.
+    • Implemented special features to satisfy clients’ every day usage and easy to
+    handle functionalities.
+    • Had meetings with team and clients to ensure best project managing criteria
+    are met while developing projects.`,
+          startDate: "2021-08-02",
+          endDate: "2021-08-30",
+        },
+        {
+          title: "Laravel Developer (Intern)",
+          company: "PONDITs",
+          desc: `• Worked on projects using Laravel knowledge gained in classes to fix bugs
+    and achieved development recognition by designing and implementing
+    special project during internship.`,
+          startDate: "2021-08-02",
+          endDate: "2021-08-30",
+        },
+        {
+          title: "Tams Agency",
+          company: "Project",
+          desc: `• Integrated b2b portal among Airline companies with their APIs to cross-connect
+    both businesses.
+    • Worked on building REST APIs and took part as project manager. Also held meetings
+    with client and front-end team on a regular basis.
+    • Built special features according to clients’ needs such as admin, agent, sub-agent
+    separate authorization; ticket booking, generating tickets, managing loans and payments,
+    quick import/export in csv/pdf, admin control over all transactions etc.`,
+          startDate: "2022-02-05",
+          endDate: "2022-07-30",
+        },
+      ],
+      skills: [
+        {
+          tool: "Problem Solving/ Programming",
+          area: "83",
+        },
+        {
+          tool: "php/ Laravel",
+          area: "92",
+        },
+        {
+          tool: "Frontend development",
+          area: "87",
+        },
+        {
+          tool: "Backend development",
+          area: "95",
+        },
+        {
+          tool: "REST API",
+          area: "89",
+        },
+        {
+          tool: "Project management/ Team lead",
+          area: "71",
+        },
+        {
+          tool: "MySql, MongoDB",
+          area: "85",
+        },
+        {
+          tool: "Vue, React, Angular",
+          area: "80",
+        },
+        {
+          tool: "Adobe Photoshop",
+          area: "89",
+        },
+        {
+          tool: "Git/ Version control",
+          area: "83",
+        },
+        {
+          tool: "vsCode, Jira, Slack",
+          area: "83",
+        },
+      ],
+      education: [
+        {
+          degree: "Bsc in Computer Science and Engineering",
+          university: "International Islamic Univeristy, Chattogram",
+          grade: "3.5",
+          batch: "2021-10-05",
+        },
+      ],
+      awards: [
+        {
+          title: "Complete Web Development Bootcamp",
+          school: "Udemy",
+        },
+      ],
+    };
   },
   methods: {
     getFormData(data) {
--- src/main.js
+++ src/main.js
@@ -5,5 +5,6 @@ import router from "./router";
 // import "./assets/main.css";
 
 import "./assets/bootstrap.min.js";
+import "./assets/fontawesome/js/all.min.js";
 
 createApp(App).use(router).mount("#app");
