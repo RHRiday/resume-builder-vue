c6089e6148bdab2d0464d111944d1494eebdd1e0
RHRiday <riffatriday@gmail.com>
2022-10-20 13:14:21 +0600

    v1.1

--- src/components/TheWelcome.vue
+++ src/components/TheWelcome.vue
@@ -341,132 +341,132 @@ export default {
     };
   },
   mounted() {
-    this.resumeData = {
-      fullName: "Rifat Hossen Riday",
-      title: "Software Engineer",
-      email: "riffatriday@gmail.com",
-      phone: "1521401417",
-      img: "",
-      location: "Halishahar, Khan Bari, Chattogram 4216.",
-      summary: `A self-taught web developer with 3 years of personal experience and 1 year of
-professional experience working with web technologies. I have worked in 8+
-professional projects as well as 20+ personal projects with technologies such as
-php/laravel, node.js/angular/react.js using MVC architecture. I have also worked with
-APIs professionally to build customized ERP/CRM/LMS based applications.`,
-      websites: [
-        "https://rhriday.github.io/",
-        "https://github.com/RHRiday",
-        "https://linkedin.com/in/rhriday",
-      ],
-      experiences: [
-        {
-          title: "Software Engineer (Fullstack)",
-          company: "Panacea PLT",
-          desc: `• Designed front-end, developed APIs and hosted web applications in the
-required environment according to client needs.
-• Implemented special features to satisfy clients’ every day usage and easy to
-handle functionalities.
-• Had meetings with team and clients to ensure best project managing criteria
-are met while developing projects`,
-          startDate: "2021-09-12",
-          endDate: "Present",
-        },
-        {
-          title: "Jr. Web Developer",
-          company: "Zorg IT Group",
-          desc: `• Designed front-end, developed APIs and hosted web applications in the
-required environment according to client needs.
-• Implemented special features to satisfy clients’ every day usage and easy to
-handle functionalities.
-• Had meetings with team and clients to ensure best project managing criteria
-are met while developing projects.`,
-          startDate: "2021-08-02",
-          endDate: "2021-08-30",
-        },
-        {
-          title: "Laravel Developer (Intern)",
-          company: "PONDITs",
-          desc: `• Worked on projects using Laravel knowledge gained in classes to fix bugs
-and achieved development recognition by designing and implementing
-special project during internship.`,
-          startDate: "2021-08-02",
-          endDate: "2021-08-30",
-        },
-        {
-          title: "Tams Agency",
-          company: "Project",
-          desc: `• Integrated b2b portal among Airline companies with their APIs to cross-connect
-both businesses.
-• Worked on building REST APIs and took part as project manager. Also held meetings
-with client and front-end team on a regular basis.
-• Built special features according to clients’ needs such as admin, agent, sub-agent
-separate authorization; ticket booking, generating tickets, managing loans and payments,
-quick import/export in csv/pdf, admin control over all transactions etc.`,
-          startDate: "2022-02-05",
-          endDate: "2022-07-30",
-        },
-      ],
-      skills: [
-        {
-          tool: "Problem Solving/ Programming",
-          area: "83",
-        },
-        {
-          tool: "php/ Laravel",
-          area: "92",
-        },
-        {
-          tool: "Frontend development",
-          area: "87",
-        },
-        {
-          tool: "Backend development",
-          area: "95",
-        },
-        {
-          tool: "REST API",
-          area: "89",
-        },
-        {
-          tool: "Project management/ Team lead",
-          area: "71",
-        },
-        {
-          tool: "MySql, MongoDB",
-          area: "85",
-        },
-        {
-          tool: "Vue, React, Angular",
-          area: "80",
-        },
-        {
-          tool: "Adobe Photoshop",
-          area: "89",
-        },
-        {
-          tool: "Git/ Version control",
-          area: "83",
-        },
-        {
-          tool: "vsCode, Jira, Slack",
-          area: "83",
-        },
-      ],
-      education: [
-        {
-          degree: "Bsc in Computer Science and Engineering",
-          university: "International Islamic Univeristy, Chattogram",
-          grade: "3.5",
-          batch: "2021-10-05",
-        },
-      ],
-      awards: [
-        {
-          title: "Complete Web Development Bootcamp",
-          school: "Udemy",
-        },
-      ],
-    };
+    //     this.resumeData = {
+    //       fullName: "Rifat Hossen Riday",
+    //       title: "Software Engineer",
+    //       email: "riffatriday@gmail.com",
+    //       phone: "1521401417",
+    //       img: "",
+    //       location: "Halishahar, Khan Bari, Chattogram 4216.",
+    //       summary: `A self-taught web developer with 3 years of personal experience and 1 year of
+    // professional experience working with web technologies. I have worked in 8+
+    // professional projects as well as 20+ personal projects with technologies such as
+    // php/laravel, node.js/angular/react.js using MVC architecture. I have also worked with
+    // APIs professionally to build customized ERP/CRM/LMS based applications.`,
+    //       websites: [
+    //         "https://rhriday.github.io/",
+    //         "https://github.com/RHRiday",
+    //         "https://linkedin.com/in/rhriday",
+    //       ],
+    //       experiences: [
+    //         {
+    //           title: "Software Engineer (Fullstack)",
+    //           company: "Panacea PLT",
+    //           desc: `• Designed front-end, developed APIs and hosted web applications in the
+    // required environment according to client needs.
+    // • Implemented special features to satisfy clients’ every day usage and easy to
+    // handle functionalities.
+    // • Had meetings with team and clients to ensure best project managing criteria
+    // are met while developing projects`,
+    //           startDate: "2021-09-12",
+    //           endDate: "Present",
+    //         },
+    //         {
+    //           title: "Jr. Web Developer",
+    //           company: "Zorg IT Group",
+    //           desc: `• Designed front-end, developed APIs and hosted web applications in the
+    // required environment according to client needs.
+    // • Implemented special features to satisfy clients’ every day usage and easy to
+    // handle functionalities.
+    // • Had meetings with team and clients to ensure best project managing criteria
+    // are met while developing projects.`,
+    //           startDate: "2021-08-02",
+    //           endDate: "2021-08-30",
+    //         },
+    //         {
+    //           title: "Laravel Developer (Intern)",
+    //           company: "PONDITs",
+    //           desc: `• Worked on projects using Laravel knowledge gained in classes to fix bugs
+    // and achieved development recognition by designing and implementing
+    // special project during internship.`,
+    //           startDate: "2021-08-02",
+    //           endDate: "2021-08-30",
+    //         },
+    //         {
+    //           title: "Tams Agency",
+    //           company: "Project",
+    //           desc: `• Integrated b2b portal among Airline companies with their APIs to cross-connect
+    // both businesses.
+    // • Worked on building REST APIs and took part as project manager. Also held meetings
+    // with client and front-end team on a regular basis.
+    // • Built special features according to clients’ needs such as admin, agent, sub-agent
+    // separate authorization; ticket booking, generating tickets, managing loans and payments,
+    // quick import/export in csv/pdf, admin control over all transactions etc.`,
+    //           startDate: "2022-02-05",
+    //           endDate: "2022-07-30",
+    //         },
+    //       ],
+    //       skills: [
+    //         {
+    //           tool: "Problem Solving/ Programming",
+    //           area: "83",
+    //         },
+    //         {
+    //           tool: "php/ Laravel",
+    //           area: "92",
+    //         },
+    //         {
+    //           tool: "Frontend development",
+    //           area: "87",
+    //         },
+    //         {
+    //           tool: "Backend development",
+    //           area: "95",
+    //         },
+    //         {
+    //           tool: "REST API",
+    //           area: "89",
+    //         },
+    //         {
+    //           tool: "Project management/ Team lead",
+    //           area: "71",
+    //         },
+    //         {
+    //           tool: "MySql, MongoDB",
+    //           area: "85",
+    //         },
+    //         {
+    //           tool: "Vue, React, Angular",
+    //           area: "80",
+    //         },
+    //         {
+    //           tool: "Adobe Photoshop",
+    //           area: "89",
+    //         },
+    //         {
+    //           tool: "Git/ Version control",
+    //           area: "83",
+    //         },
+    //         {
+    //           tool: "vsCode, Jira, Slack",
+    //           area: "83",
+    //         },
+    //       ],
+    //       education: [
+    //         {
+    //           degree: "Bsc in Computer Science and Engineering",
+    //           university: "International Islamic Univeristy, Chattogram",
+    //           grade: "3.5",
+    //           batch: "2021-10-05",
+    //         },
+    //       ],
+    //       awards: [
+    //         {
+    //           title: "Complete Web Development Bootcamp",
+    //           school: "Udemy",
+    //         },
+    //       ],
+    //     };
   },
   methods: {
     getFormData(data) {
